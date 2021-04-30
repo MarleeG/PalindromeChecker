68e9bff142775d7328a76117d4c811be1c6ea3a7
Marlee Gerard <gerardchristina42@gmail.com>
2021-04-29 21:17:09 -0400

    updated readme file and adjusted server.js file for heroku

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+  });
+}
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, (err) => {
