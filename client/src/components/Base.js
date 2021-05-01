78af53cc5fd0b31e420641568d69044ceb1e2775
Marlee Gerard <marleegerard@gmail.com>
2021-04-30 21:50:45 -0400

    adding input field

---
+++ client/src/components/Base.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./Header";
+import InputField from "./InputField";
+
+import "./Base.css";
+const Base = () => {
+  return (
+    <div className="my-5 base">
+      <Container>
+        <Header />
+        <InputField />
+      </Container>
+    </div>
+  );
+};
+
+export default Base;
