59205231e1f7a4a3f5180fd87829993e9e7b37a9
Marlee Gerard <gerardchristina42@gmail.com>
2021-05-10 22:32:25 -0400

    added google fonts to header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header text-center">
-      <h1>PALINDROME CHECKER</h1>
+      <h1 className="g-font-amatic">PALINDROME CHECKER</h1>
       <p>
         A word, phrase, or sequence that reads the same backward as forward,
         e.g., madam or nurses run.
