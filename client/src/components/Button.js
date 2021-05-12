8151c05646206efde4c9c5c394041b201d9bb82d
Marlee Gerard <gerardchristina42@gmail.com>
2021-05-11 22:17:52 -0400

    styled check palindrome button

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -4,7 +4,7 @@ import "./Button.css";
 const Button = (props) => {
   const { disabled } = props;
   return (
-    <button type="submit" className="text-center button my-3" disabled={disabled}>
+    <button type="submit" className="text-center button my-3 g-font-amatic" disabled={disabled}>
       check palindrome
     </button>
   );
